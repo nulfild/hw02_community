cc6899e7b1ff60c535a2f27ab2e124852739907a
German Kunin <kunin.german@icloud.com>
2022-02-08 19:39:37 +0300

    The first attempt to do homework
    First try...

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# backend_community_homework
-
-[![CI](https://github.com/yandex-praktikum/hw02_community/actions/workflows/python-app.yml/badge.svg?branch=master)](https://github.com/yandex-praktikum/hw02_community/actions/workflows/python-app.yml)
+# Yatube
+### Описание
+Благодаря этому проекту можно будет угощать мороженым друзей из разных городов.
+### Технологии
+Python 3.7
+Django 2.2.19
+### Запуск проекта в dev-режиме
+- Установите и активируйте виртуальное окружение
+- Установите зависимости из файла requirements.txt
+```
+pip install -r requirements.txt
+``` 
+- В папке с файлом manage.py выполните команду:
+```
+python3 manage.py runserver
+```
+### Авторы
+Nulfild (German Kunin)
---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'Записи сообщества ' + group.title
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
